afe2b9fd740fb8571dfb57fc12118cd4b1476409
I3OR2A <f74012227@mail.ncku.edu.tw>
2020-02-22 17:38:12 +0800

    4-8Storage封装

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
   <div id="app">
-     <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+import storage from './storage'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    
   },
   data(){
     return {
-      
+      a:''
     }
   },
   mounted(){
-    
+    // storage.setItem('a',1);
+    // storage.setItem('b',{a:1});
+    // storage.setItem('c',10,'b');
+    // this.a = storage.getItem('b');
+    // storage.clear('a');
+    storage.clear('a','b');
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
